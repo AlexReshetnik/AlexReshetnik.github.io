8b175cb30e28ba82961c68702493f1ca59accea1
AlexReshetnik <reshetnik262@gmail.com>
2022-11-20 12:16:33 +0200

    Updates

---
+++ precache-manifest.f7d5d7a4bb00c0b4df0afe8a447b77ed.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ba5ad989d47b842851",
+    "url": "/Summary_React_Redux/static/css/main.678a6290.chunk.css"
+  },
+  {
+    "revision": "60ba5ad989d47b842851",
+    "url": "/Summary_React_Redux/static/js/main.60ba5ad9.chunk.js"
+  },
+  {
+    "revision": "b77fbe414db0c47536d4",
+    "url": "/Summary_React_Redux/static/js/1.b77fbe41.chunk.js"
+  },
+  {
+    "revision": "fd6f1657ffeb2dfbd0de",
+    "url": "/Summary_React_Redux/static/js/runtime~main.fd6f1657.js"
+  },
+  {
+    "revision": "857e66cc18fc68e997d7dd6706793b3f",
+    "url": "/Summary_React_Redux/static/media/github.857e66cc.png"
+  },
+  {
+    "revision": "2b667799870fc97d0540659e6d251df4",
+    "url": "/Summary_React_Redux/static/media/competence.2b667799.png"
+  },
+  {
+    "revision": "a98b1a959e3bbd484720d1abddc477f2",
+    "url": "/Summary_React_Redux/static/media/task.a98b1a95.png"
+  },
+  {
+    "revision": "3cce4cc7cc9c6b5380702030caf964ac",
+    "url": "/Summary_React_Redux/static/media/contact.3cce4cc7.png"
+  },
+  {
+    "revision": "ca750ad37ca9a205874d31abc921e1d0",
+    "url": "/Summary_React_Redux/static/media/main3.ca750ad3.png"
+  },
+  {
+    "revision": "1ecc5b013fcfcefc1ac3ea96e9f29277",
+    "url": "/Summary_React_Redux/static/media/web_summary_react.1ecc5b01.mov"
+  },
+  {
+    "revision": "5c2519bd473d7653733ebcb5ae4300d0",
+    "url": "/Summary_React_Redux/static/media/web_summary.5c2519bd.mov"
+  },
+  {
+    "revision": "125f072b7e6d845047679cd599330e17",
+    "url": "/Summary_React_Redux/static/media/tourism-company.125f072b.mov"
+  },
+  {
+    "revision": "78f76e7eb5e021b5c2229a0300185112",
+    "url": "/Summary_React_Redux/static/media/Method_kruskala.78f76e7e.mov"
+  },
+  {
+    "revision": "d3fe5a0035c261f05973801c89b7b41c",
+    "url": "/Summary_React_Redux/static/media/Game_aim_training.d3fe5a00.mov"
+  },
+  {
+    "revision": "778ab1fb72acbb8db21c7a4fa1509156",
+    "url": "/Summary_React_Redux/static/media/Calendar_TODO.778ab1fb.mov"
+  },
+  {
+    "revision": "bfa1ed85c355f5710970da52e02a3f8e",
+    "url": "/Summary_React_Redux/static/media/Extensions_Age_go.bfa1ed85.mov"
+  },
+  {
+    "revision": "746f5cc83a42b24c07d8d4d3a97d2010",
+    "url": "/Summary_React_Redux/static/media/Extension_ToDo.746f5cc8.mov"
+  },
+  {
+    "revision": "d9240f480dd4ab960e0204481434be15",
+    "url": "/Summary_React_Redux/static/media/Sheets_API_Quickstart.d9240f48.mov"
+  },
+  {
+    "revision": "53ad233fce709a424e4bb502b0417d39",
+    "url": "/Summary_React_Redux/static/media/Google_authentication.53ad233f.mov"
+  },
+  {
+    "revision": "e7640c42a58380c22d9f409ea68f108a",
+    "url": "/Summary_React_Redux/static/media/pinterest.e7640c42.png"
+  },
+  {
+    "revision": "a34d9c30632e7a7f9eb64ba6a08eba21",
+    "url": "/Summary_React_Redux/static/media/telegram.a34d9c30.png"
+  },
+  {
+    "revision": "b007a9ed75d9238776ee20134323f0c2",
+    "url": "/Summary_React_Redux/static/media/mail.b007a9ed.png"
+  },
+  {
+    "revision": "f8411e8c253a55b9f51b407835f42fc7",
+    "url": "/Summary_React_Redux/index.html"
+  }
+];
