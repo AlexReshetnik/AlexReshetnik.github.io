0cda90f34b1af274d39e43232ac9a2111ef62f44
AlexReshetnik <reshetnik262@gmail.com>
2022-11-20 12:13:51 +0200

    test build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7a329dfa823609b718a36ac517f0e84.js"
+  "/Summary_React_Redux/precache-manifest.f7d5d7a4bb00c0b4df0afe8a447b77ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Summary_React_Redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
